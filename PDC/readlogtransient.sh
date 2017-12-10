eb6ccaeb31dca75065591488747264b8eb9a3d30
yuehan_wang <34420665+dehhrvetuj@users.noreply.github.com>
2017-12-10 19:24:29 +0100

    Update readlogtransient.sh

--- PDC/readlogtransient.sh
+++ PDC/readlogtransient.sh
@@ -5,15 +5,15 @@ gnuplot -persist <<-EOFMarker
 	set ylabel 'Value'
 	set xlabel 'Time Step'
 	set key outside
-#   set yrange [1E-8:1]
-#   set xrange [4779:5100]
-  set grid
+#	set yrange [1E-8:1]
+#	set xrange [4779:5100]
+	set grid
 
 	titles="time-step flow-time report1 report2 report3"
 
 #	plot for [col=2:8] "< cat $1 | grep -E  '$rexp' "  using 1:col title word(titles,col) with lines
 
-    plot for [col=3:5] "< cat $1 | grep -E  '$rexp' " using 1:col title word(titles,col) with lines
+	plot for [col=3:5] "< cat $1 | grep -E  '$rexp' " using 1:col title word(titles,col) with lines
 	
 	pause 1
     
