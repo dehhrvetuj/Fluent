ff92541851337732aaa3e3edb1b3fbf021436763
yuehan_wang <34420665+dehhrvetuj@users.noreply.github.com>
2017-12-18 14:47:02 +0100

    Update age_of_air.c

--- UDF/age_of_air.c
+++ UDF/age_of_air.c
@@ -1,12 +1,17 @@
 #include "udf.h"
 #include "prop.h"
 
+/* Age of air calculation */
+/* 2017-12-17		*/	
+
+/* DT/Dt = diffusition-term + soruce-term */
+/* https://www.computationalfluiddynamics.com.au/tips-tricks-calculating-mean-age-of-air-with-cfd/  */
+
 
 DEFINE_DIFFUSIVITY(mean_age_diff, c, t, i)
 {
 	return C_R(c,t) * 2.88e-05 + C_MU_EFF(c,t) / 0.7;
 }
-
 
 
 DEFINE_SOURCE(mean_age_source, c, t, dS, eqn)
