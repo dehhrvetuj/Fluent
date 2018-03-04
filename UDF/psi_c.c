c6a6abe85f67379c7de869deefad9013eace1d23
yuehan_wang <34420665+dehhrvetuj@users.noreply.github.com>
2018-03-04 19:33:07 +0100

    Update psi_c.c

--- UDF/psi_c.c
+++ UDF/psi_c.c
@@ -82,3 +82,36 @@ DEFINE_DPM_SCALAR_UPDATE(Part_Con, c, t, initialize, p)
 
 	C_UDMI(c,t,1) += P_FLOW_RATE(p)*P_DT(p)/C_VOLUME(c,t);
 }
+
+
+#include "udf.h"
+
+DEFINE_ON_DEMAND(particle_cells_count)
+{
+	Injection *Ilist;
+	Injection *I;
+	Particle *p;
+	cell_t c;
+	Thread *c_t;
+	int parcel_trapped = 0;
+	int num_part = 0;
+	int number_in_parcel = 0;
+
+	Ilist = Get_dpm_injections();
+
+	loop(I, Ilist)
+	{
+		loop(p, I->p)
+		{
+			c = P_CELL(p);
+			c_t = P_CELL_THREAD(p);
+			parcel_trapped+=1;
+			num_part=p->number_in_parcel;
+		}
+	}
+
+	Message("num_part: %d\n",num_part);
+	Message("parcel_trapped: %d\n",parcel_trapped);
+	Message("number_in_parcel: %d\n",number_in_parcel);
+
+}
