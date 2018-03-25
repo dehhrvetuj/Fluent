0809c751eaa2fe5e0aaa0118a1a2966b1a24c709
yuehan_wang <34420665+dehhrvetuj@users.noreply.github.com>
2018-03-25 17:26:20 +0200

    Update dpm_bc.c

--- UDF/DPM/dpm_bc.c
+++ UDF/DPM/dpm_bc.c
@@ -1,8 +1,73 @@
 #include "udf.h"
+#include "dpm.h"
+
+static int version = 1;
+static int release = 2;
+
+static int count = 0;
+
+DEFINE_EXECUTE_ON_LOADING(report_version, libname)
+{
+    Message0("\nLoading %s version %d.%d\n",libname,version,release);
+	
+	count = 0;
+	/*
+	Set_User_Memory_Name(0,"DPM_CON");
+	Set_User_Memory_Name(1,"ResidenceTime"); */
+}
+
+DEFINE_ON_DEMAND(on_demand_calc)
+{
+   Domain *d; /* declare domain pointer since it is not passed as an  
+                 argument to the DEFINE macro  */
+   Thread *t;
+   cell_t c;
+   
+   d = Get_Domain(1);     /* Get the domain using Fluent utility */
+
+   /* Loop over all cell threads in the domain */
+   thread_loop_c(t,d)
+   {	 
+		/* Loop over all cells  */
+		begin_c_loop(c,t)
+		{ 
+		/*
+		   C_UDMI(c,t,0) = 0.0;
+		   C_UDMI(c,t,1) = 0.0; */
+		}
+		end_c_loop(c,t)
+ 
+   }
+      
+   Message0("\nUser Defined Memory is now initialized!\n"); 
+   
+   strcpy(user_particle_vars[0].name,"melting-index");
+   strcpy(user_particle_vars[0].label,"Melting Index");
+
+}
+
+DEFINE_DPM_SCALAR_UPDATE(Part_Con, c, t, initialize, p)
+{
+  if (initialize)
+    {
+     /* this is the initialization call, set:
+      * p->user[0] contains the melting index, initialize to 0 
+      * viscosity_0 contains the viscosity at the start of a time step*/
+
+     P_USER_REAL(p,0) = 0.; 
+
+    }
+
+  else
+    {
+		Message("\nParticle Time Step %e in cell ID %d with time0 %e\n", P_DT(p), c, P_TIME0(p)); 
+    }	
+	
+}
 
 DEFINE_DPM_BC(bc_reflect, p, t, f, f_normal, dim)
 {
-	real alpha;  /* angle of particle path with face normal */ 
+	real alpha = 0.0;  /* angle of particle path with face normal */ 
    	
 	real vn = 0.0;
 
@@ -10,12 +75,12 @@ DEFINE_DPM_BC(bc_reflect, p, t, f, f_normal, dim)
 	real tan_coeff = 0.1;
 	
 	real normal[3];
-	int i, idim = dim;
+	int i = 0, idim = dim;
 	
-	real NV_VEC(x);
-
+	P_USER_REAL(p,0) += 1;
 	
-	if(P_TIME(p)>0.5)
+	/* Particle residence time > 10 second or number of impacts > 100 */
+	if( P_TIME(p) > 10  || P_USER_REAL(p,0) > 100 )
 	{
 		return PATH_END;
 	}
