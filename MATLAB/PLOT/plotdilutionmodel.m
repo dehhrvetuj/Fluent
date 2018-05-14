7549bedb911d1cc507af414758e296401470b0f0
yuehan_wang <34420665+dehhrvetuj@users.noreply.github.com>
2018-05-14 18:27:52 +0200

    Update plotdilutionmodel.m

--- MATLAB/PLOT/plotdilutionmodel.m
+++ MATLAB/PLOT/plotdilutionmodel.m
@@ -62,8 +62,13 @@ for i=1:NUM
     
     if i==1
         scatter(OT(1:3,1),OT(1:3,2), markersize,'b','filled','s'); %,'LineWidth',6);
+        line(OT(1:3,1),OT(1:3,2),'LineStyle','--','LineWidth',linewidth-1,'Color','b');
+        
         scatter(OT(4:6,1),OT(4:6,2), markersize,'r','filled','^');
+        line(OT(4:6,1),OT(4:6,2),'LineStyle','--','LineWidth',linewidth-1,'Color','r');
+        
         scatter(OT(7:9,1),OT(7:9,2), markersize,'g','filled','d'); %,'LineWidth',12)
+        line(OT(7:9,1),OT(7:9,2),'LineStyle','--','LineWidth',linewidth-1,'Color','g');
 
         set(gca,'YLim',[0.01 100]);
         text(0.3,1.1,'Operating Table','FontSize',fontsize,'FontWeight','bold','Color','k','Units','normalized');
@@ -71,8 +76,13 @@ for i=1:NUM
     
     if i==2
         scatter(IT1(1:3,1),IT1(1:3,2), markersize,'b','filled','s');
+        line(IT1(1:3,1),IT1(1:3,2),'LineStyle','--','LineWidth',linewidth-1,'Color','b');
+        
         scatter(IT1(4:6,1),IT1(4:6,2), markersize,'r','filled','^');
+        line(IT1(4:6,1),IT1(4:6,2),'LineStyle','--','LineWidth',linewidth-1,'Color','r');
+        
         scatter(IT1(7:9,1),IT1(7:9,2), markersize,'g','filled','d'); 
+        line(IT1(7:9,1),IT1(7:9,2),'LineStyle','--','LineWidth',linewidth-1,'Color','g');
 
         set(gca,'YLim',[0.1 100]);
         text(0.27,1.1,'Instrument Table 1','FontSize',fontsize,'FontWeight','bold','Color','k','Units','normalized');
@@ -80,8 +90,13 @@ for i=1:NUM
     
     if i==3
         scatter(IT2(1:3,1),IT2(1:3,2), markersize,'b','filled','s');
+        line(IT2(1:3,1),IT2(1:3,2),'LineStyle','--','LineWidth',linewidth-1,'Color','b');
+        
         scatter(IT2(4:6,1),IT2(4:6,2), markersize,'r','filled','^');
+        line(IT2(4:6,1),IT2(4:6,2),'LineStyle','--','LineWidth',linewidth-1,'Color','r');
+        
         scatter(IT2(7:9,1),IT2(7:9,2), markersize,'g','filled','d');
+        line(IT2(7:9,1),IT2(7:9,2),'LineStyle','--','LineWidth',linewidth-1,'Color','g');
 
         set(gca,'YLim',[10 100]);
         text(0.27,1.1,'Instrument Table 2','FontSize',fontsize,'FontWeight','bold','Color','k','Units','normalized');
@@ -92,8 +107,9 @@ for i=1:NUM
 %   axis([0.8 4 0.01 100]);
     axs.XRuler.MinorTick = 0.4:0.1:4;
     set(gca,'XLim',[0.7 4],'XTick',[1 2 3 4]);
+%     set(gca,'YLim',[0 80]);
 %   xticks([1 2 3 4]) % Matlab 17.2 or later
-%     legend('Mixing','TAF','LAF','Analytical');
+%   legend('Mixing','TAF','LAF','Analytical');
 
    
     
@@ -102,23 +118,21 @@ end
 % print('XXXXX','-dmeta')
 % print(gcf,'foo.png','-dpng','-r300');         % 300 dpi
     
-    
-% 
-%     
-% axs = subplot(1,3,1,'Position',[gap_left, gap_bottom, width, height]);
-% 
-% % subplot(1,3,1)
-% xlabel('Airflow Rate [m^3/s]','FontSize',8,'FontWeight','bold','Color','k');
-% ylabel('CFU / m^3','FontSize',8,'FontWeight','bold','Color','k');
-% box on
-% 
-% subplot(1,3,2,'Position',[gap_left+space, gap_bottom, width, height])
-% xlabel('Airflow Rate','FontSize',8,'FontWeight','bold','Color','k');
-% ylabel('Z [m]','FontSize',8,'FontWeight','bold','Color','k');
-% box on
-% 
-% subplot(1,3,3,'Position',[gap_left+space*2, gap_bottom, width, height])
-% xlabel('Airflow Rate','FontSize',8,'FontWeight','bold','Color','k');
-% ylabel('Z [m]','FontSize',8,'FontWeight','bold','Color','k');
-% box on
-%    
+
+figure(2)
+hold on
+% plot(OT(1:3,1),OT(1:3,2),'sb--','LineWidth', 0.5,'Marker','s','MarkerSize',8,'MarkerEdgeColor','b','MarkerFaceColor','b');
+
+scatter(OT(1:3,1),OT(1:3,2), markersize,'b','filled','s'); %,'LineWidth',6);      
+% line(OT(1:3,1),OT(1:3,2),'LineStyle','--','LineWidth',linewidth-1,'Color','b');
+scatter(OT(4:6,1),OT(4:6,2), markersize,'r','filled','^');
+scatter(OT(7:9,1),OT(7:9,2), markersize,'g','filled','d'); %,'LineWidth',12)
+plot(Q,C,'k--','LineWidth',linewidth-1)
+legend('Mixing','TAF','LAF','Analytical','Location','northoutside','Orientation','horizontal');
+legend('boxoff');
+
+% [~,objects] = legend('Mixing','TAF','LAF','Analytical');
+% objects(2).LineStyle = '-';
+
+
+  
