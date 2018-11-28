31c73e04c4679bddc09bcba32732fae47b774116
yuehan_wang <34420665+dehhrvetuj@users.noreply.github.com>
2018-11-28 14:25:40 +0100

    Update readdpmtrack.sh

--- PDC/readdpmtrack.sh
+++ PDC/readdpmtrack.sh
@@ -1,6 +1,6 @@
 grep -E '^number tracked =[[:blank:]]+[[:digit:]]{2,9}' unsteady.log
 
-echo 'hello world'|grep -oP '(?<=hello\s)\w+'   #是找出以hello为前缀之后的字符串
+echo 'hello world'|grep -oP '(?<=number tracked =\s)\w+'   #是找出以hello为前缀之后的字符串
 
 
 gnuplot -persist <<-EOFMarker
