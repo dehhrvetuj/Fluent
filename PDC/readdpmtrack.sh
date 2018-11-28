411ead53faa670846a978ac533aa318833816711
yuehan_wang <34420665+dehhrvetuj@users.noreply.github.com>
2018-11-28 14:18:24 +0100

    Update readdpmtrack.sh

--- PDC/readdpmtrack.sh
+++ PDC/readdpmtrack.sh
@@ -1,3 +1,18 @@
 grep -E '^number tracked =[[:blank:]]+[[:digit:]]{2,9}' unsteady.log
 
 echo 'hello world'|grep -oP '(?<=hello\s)\w+'   #是找出以hello为前缀之后的字符串
+
+
+gnuplot -persist <<-EOFMarker
+
+
+    set logscale y
+    set grid
+  
+    plot for [col=1] "< cat $1 | grep -E '$rexp' "
+  
+  
+  pause 1
+                                  
+  exit
+gnu
