8f9239a069e2b22ba97f7b232b98326c1ef84705
yuehan_wang <34420665+dehhrvetuj@users.noreply.github.com>
2018-12-04 11:09:02 +0100

    Update set-door-wall.scm

--- SCHEME/DOOR/set-door-wall.scm
+++ SCHEME/DOOR/set-door-wall.scm
@@ -16,6 +16,15 @@
 ;
 ;
 ;
+; SET DOOR FROM INTERIOR TYPE TO INTERIOR TYPE
+; (do ((x 1 (+ x 1))) ((> x 50))
+	; (ti-menu-load-string 
+		; (format #f "/define/boundary-conditions/modify-zones/zone-type xdoor-d~02d interior" x)
+	; )
+; )
+;
+;
+;
 ; SET DOOR TO WALL AND CHANGE CORRESPONDING WALL BOUNDARY CONDITIONS
 (do ((x 1 (+ x 1))) ((> x 50))
 	; (ti-menu-load-string 
