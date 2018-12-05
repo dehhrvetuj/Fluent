21888e33eacd1683f4b9a9ed02e310efe2b0fa2a
yuehan_wang <34420665+dehhrvetuj@users.noreply.github.com>
2018-12-05 11:17:44 +0100

    Add files via upload

---
+++ SCHEME/controlruntime.scm
@@ -0,0 +1,18 @@
+;;
+;;
+;; CHANGE THE FREQUENCY OF LOADING THIS SCHEME FILE
+(ti-menu-load-string  "/solve/execute-commands add-edit control-run-time 10 \"iteration\"  \"/file/read-macros controlruntime.scm\"  ")
+;; (ti-menu-load-string  "/solve/execute-commands add-edit control-run-time 10 \"time-step\"  \"/file/read-macros controlruntime.scm\"  ")
+;;
+;;
+;;
+;; WRITE DAT FILE
+(ti-menu-load-string  "/file/write-data  ./temp.gz")
+;;
+;;
+;;
+;;
+;;
+;;
+;;
+
